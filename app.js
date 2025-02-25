50b8fea16e111ca69b51efc13a64ffc0002f4572
Wdrew232 <wdrew232@gmail.com>
2025-02-25 02:35:23 +0000

    finished jest test

---
+++ app.js
@@ -0,0 +1,31 @@
+// We declare the function with the exact name "fromEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.07;
+    // return the dollar value
+    return valueInDollar;
+}
+
+const fromDollarToYen = function(valueinDollar) {
+
+    let valueInDollar = valueinDollar * 156.5;
+    return valueInDollar;
+}
+
+const fromYenToPound = function(valueinYen) {
+    
+    let valueinpound = valueinYen * .87;
+    return valueinpound;
+}
+
+// This is my function that sums two numbers
+const sum = (a,b) => {
+    return a + b
+}
+
+// Just a console log for ourselves
+console.log(sum(7,3))
+
+// Export the function to be used on other files 
+// (similar to the keyword "export" when using webpack)
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound  };
